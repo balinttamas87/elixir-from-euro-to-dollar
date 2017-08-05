bada93052fae8f98ef8f05cb8ae9dc2c330da649
Tamas Balint <balint.tamas87@gmail.com>
2017-08-05 12:35:35 +0100

    make HTTP calls with the HTTPoison library

--- lib/Budget/conversion.ex
+++ lib/Budget/conversion.ex
@@ -1,5 +1,9 @@
 defmodule Budget.Conversion do
 	def from_euro_to_dollar(amount) do
-
+		url = "cs-currency-rates.herokuapp.com/currency-rates"
+		case HTTPoison.get(url) do
+			{:ok, response} -> parse(response) |> convert(amount)
+			{:error, _} -> "Error fetching rates" 
+		end
 	end
 end
